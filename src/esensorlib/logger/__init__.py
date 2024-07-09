d1a0b9a714e4e57d7e443086988aaf88979c0f33
cubicleguy <86331043+cubicleguy@users.noreply.github.com>
2024-07-09 10:15:33 -0700

    Delete src/esensorlib/logger directory
    replaced by example directory

--- src/esensorlib/logger/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Logger Script and LoggerHelper Library
-============
-
-Provides an example logger script with LoggerHelper library as an example
-conole application of the esensorlib package
-"""
